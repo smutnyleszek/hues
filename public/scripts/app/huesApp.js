26d3750417fc3228391f70b6fe433309066a6aaf
Leszek Pietrzak <lpietrzak@gog.com>
2015-12-14 14:07:10 +0100

    Further learning Backbone - working on backbone architecture

--- public/scripts/app/huesApp.js
+++ public/scripts/app/huesApp.js
@@ -1,16 +1,40 @@
-(function() {
-  var AppView, app;
+var Space, SpacesView, spaces, testSpace;
 
-  AppView = Backbone.View.extend({
-    el: '#app',
-    initialize: function() {
-      return this.render();
-    },
-    render: function() {
-      return this.$el.html("Huello world!");
-    }
-  });
+Space = Backbone.Model.extend({
+  defaults: {
+    label: 'foo bar:',
+    slug: 'dupa'
+  }
+});
 
-  app = new AppView();
+testSpace = new Space({
+  slug: 'test1'
+});
 
-}).call(this);
+SpacesView = Backbone.View.extend({
+  el: '#spaces',
+  template: _.template($('#space-template').html()),
+  events: {
+    'click button[js-space-button]': 'onButtonClick',
+    'keypress input[js-space-input]': 'onKeypress'
+  },
+  initialize: function() {
+    this.$el = $(this.el);
+    return this.render();
+  },
+  render: function() {
+    this.$el.html(this.template(this.model.attributes));
+    this.input = this.$('input[js-space-input]');
+    return this;
+  },
+  onButtonClick: function() {
+    return console.log('button click');
+  },
+  onKeypress: function() {
+    return console.log('key pressed');
+  }
+});
+
+spaces = new SpacesView({
+  model: testSpace
+});
