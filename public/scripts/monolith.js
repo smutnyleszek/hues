dd19c2a0409c8121bfc7a1ad12cbdf3a14bb110d
Leszek Pietrzak <smutnyleszek@gmail.com>
2016-02-27 02:56:12 +0100

    grunt to NPM switch - using NPM as a builder and task runner - ditched grunt completely

--- public/scripts/monolith.js
+++ public/scripts/monolith.js
@@ -1,4 +1,7 @@
-/* 2016-02-26 */
-'use strict';
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";function _interopRequireDefault(o){return o&&o.__esModule?o:{"default":o}}var _foo=require("./foo.js"),_foo2=_interopRequireDefault(_foo);console.log("foo");
 
-console.log('dupa');
+},{"./foo.js":2}],2:[function(require,module,exports){
+"use strict";function _classCallCheck(e,n){if(!(e instanceof n))throw new TypeError("Cannot call a class as a function")}var _createClass=function(){function e(e,n){for(var a=0;a<n.length;a++){var r=n[a];r.enumerable=r.enumerable||!1,r.configurable=!0,"value"in r&&(r.writable=!0),Object.defineProperty(e,r.key,r)}}return function(n,a,r){return a&&e(n.prototype,a),r&&e(n,r),n}}(),Foo=function(){function e(){_classCallCheck(this,e)}return _createClass(e,[{key:"log",value:function(){console.log("bar")}}]),e}();
+
+},{}]},{},[1]);
