a8f8872e45170830740d4cb2e394eba53b054498
Leszek Pietrzak <lpietrzak@gog.com>
2016-02-29 16:16:13 +0100

    set huge eslint config

--- _assets/scripts/tests/appSpec.js
+++ _assets/scripts/tests/appSpec.js
@@ -1,7 +1,11 @@
-describe('test', function () {
-
-    it('foo', function () {
+describe('test', () => {
+    // first test
+    it('foo', () => {
         expect('foo').toEqual('foo');
     });
 
+    // second test
+    it('bar', () => {
+        expect('bar').toEqual('bar');
+    });
 });
