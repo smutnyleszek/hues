2b83d0709a7a32fd27c314ab059244af618f5aeb
Leszek Pietrzak <lpietrzak@gog.com>
2016-09-09 15:26:39 +0200

    use karma for testing and setup requirejs environment for it

---
+++ .karma.conf.js
@@ -0,0 +1,52 @@
+module.exports = function (config) {
+    config.set({
+        basePath: '',
+        port: 9876,
+        autoWatch: false,
+        singleRun: true,
+
+        reporters: ['spec', 'coverage'],
+        logLevel: config.LOG_INFO,
+        colors: true,
+
+        specReporter: {
+            showSpecTiming: true
+        },
+
+        frameworks: ['jasmine', 'requirejs'],
+        browsers: ['PhantomJS'],
+
+        files: [
+            './node_modules/phantomjs-polyfill/bind-polyfill.js',
+            './node_modules/babel-polyfill/dist/polyfill.min.js',
+            {pattern: 'src/scripts/**/*.js', included: false},
+            {pattern: 'test/scripts/**/*Spec.js', included: false},
+            'test/scripts/init.js'
+        ],
+        exclude: [
+            'src/scripts/main.js'
+        ],
+
+        // preprocess matching files before serving them to the browser
+        // https://npmjs.org/browse/keyword/karma-preprocessor
+        preprocessors: {
+            'src/scripts/**/*.js': ['babel', 'coverage'],
+            'test/scripts/**/*Spec.js': ['babel']
+        },
+
+        coverageReporter: {
+            dir: 'test/coverage',
+            reporters: [
+                {
+                    type: 'html',
+                    includeAllSources: true
+                },
+                {
+                    type: 'text-summary',
+                    file: 'text-summary.txt',
+                    includeAllSources: true
+                }
+            ]
+        }
+    });
+};
