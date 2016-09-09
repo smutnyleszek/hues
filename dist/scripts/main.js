c0f81de611335facb439e80885f83e37e2102265
Leszek Pietrzak <lpietrzak@gog.com>
2016-09-09 16:19:15 +0200

    working react setup with reactDOM

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,13 +1,19 @@
-define(['./Helper'], function (_Helper) {
-  'use strict';
+define(['./Helper', 'react', 'react-dom'], function (_Helper, _react, _reactDom) {
+    'use strict';
 
-  var _Helper2 = _interopRequireDefault(_Helper);
+    var _Helper2 = _interopRequireDefault(_Helper);
 
-  function _interopRequireDefault(obj) {
-    return obj && obj.__esModule ? obj : {
-      default: obj
-    };
-  }
+    var _react2 = _interopRequireDefault(_react);
 
-  console.log(new _Helper2.default('test'));
+    var _reactDom2 = _interopRequireDefault(_reactDom);
+
+    function _interopRequireDefault(obj) {
+        return obj && obj.__esModule ? obj : {
+            default: obj
+        };
+    }
+
+    console.log(new _Helper2.default('test'));
+
+    _reactDom2.default.render(_react2.default.createElement('div', { label: 'test' }, 'Hello World!'), document.getElementById('app'));
 });
