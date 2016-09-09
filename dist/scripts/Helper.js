cc1e4f8d4bc914c35d5099a138c47f611198841d
Leszek Pietrzak <lpietrzak@gog.com>
2016-09-09 14:53:57 +0200

    more cleanups, script building with requirejs

---
+++ dist/scripts/Helper.js
@@ -0,0 +1,21 @@
+define(["exports"], function (exports) {
+    "use strict";
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+
+    function _classCallCheck(instance, Constructor) {
+        if (!(instance instanceof Constructor)) {
+            throw new TypeError("Cannot call a class as a function");
+        }
+    }
+
+    var Helper = function Helper(foo) {
+        _classCallCheck(this, Helper);
+
+        this._foo = foo;
+    };
+
+    exports.default = Helper;
+});
