a86a496840a39adb745a19a3303fe8594db31583
Leszek Pietrzak <lpietrzak@gog.com>
2016-09-16 16:36:51 +0200

    created deepFreeze and spaces data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,9 @@
 import CurrentColor from 'components/CurrentColor';
 import React from 'react';
 import ReactDom from 'react-dom';
+import spacesData from 'data/spaces';
+
+console.log(spacesData);
 
 ReactDom.render(
     React.createElement(CurrentColor),
