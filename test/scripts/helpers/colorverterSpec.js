82d9c318af1efbbcb5dc08de235e9bc74310d67d
Leszek Pietrzak <smutnyleszek@gmail.com>
2016-10-03 14:49:52 +0200

    fixed test

--- test/scripts/helpers/colorverterSpec.js
+++ test/scripts/helpers/colorverterSpec.js
@@ -1,20 +1,25 @@
 import colorverter from 'helpers/colorverter';
-import deepFreeze from 'helpers/deepFreeze';
 
-const mocks = deepFreeze({
-    validOne: {
-        hex: ['00', '80', 'ff'],
-        rgb: [0, 128, 255],
-        hsl: [210, 100, 50],
-        hwb: [210, 0, 0]
-    },
-    validTwo: {
-        hex: ['00', '7f', 'ff'],
-        rgb: [0, 127, 255],
-        hsl: [210, 100, 50],
-        hwb: [210, 0, 0]
-    }
-});
+const testPairs = [
+    ['hex', ['00', '7f', 'ff'], 'rgb', [0, 127, 255]],
+    ['hex', ['00', '7f', 'ff'], 'hsl', [210, 100, 50]],
+    ['hex', ['00', '7f', 'ff'], 'hwb', [210, 0, 0]],
+    ['rgb', [0, 127, 255], 'hex', ['00', '7f', 'ff']],
+    ['rgb', [0, 127, 255], 'hwb', [210, 0, 0]],
+    ['hsl', [210, 100, 50], 'hex', ['00', '7f', 'ff']],
+    ['hsl', [210, 100, 50], 'rgb', [0, 127, 255]],
+    ['hsl', [210, 100, 50], 'hwb', [210, 0, 0]],
+    ['hwb', [210, 0, 0], 'hex', ['00', '80', 'ff']],
+    ['hwb', [210, 0, 0], 'rgb', [0, 128, 255]],
+    ['hwb', [210, 0, 0], 'hsl', [210, 100, 50]],
+    // there are some RGB color variations that can't be displayed with HSL:
+    ['rgb', [0, 125, 255], 'hsl', [211, 100, 50]],
+    ['rgb', [0, 126, 255], 'hsl', [210, 100, 50]],
+    ['rgb', [0, 127, 255], 'hsl', [210, 100, 50]],
+    ['rgb', [0, 128, 255], 'hsl', [210, 100, 50]],
+    ['rgb', [0, 129, 255], 'hsl', [210, 100, 50]],
+    ['rgb', [0, 130, 255], 'hsl', [209, 100, 50]]
+];
 
 const hexConvertersMap = new Map([
     ['rgb', 'hexToRgb'],
@@ -57,6 +62,11 @@ const generatorsMap = new Map([
     ['hwb', 'getRandomHwb']
 ]);
 
+function getConverter(from, to) {
+    const targetsMap = convertersMap.get(from);
+    return targetsMap.get(to);
+}
+
 // -----------------------------------------------------------------------------
 // testing returned values
 // -----------------------------------------------------------------------------
@@ -66,7 +76,11 @@ describe('colorverter.*To*', () => {
         convertersMap.forEach((targetsMap, sourceName) => {
             targetsMap.forEach((convertName, targetName) => {
                 const convert = colorverter[convertName].bind(colorverter);
-                const newColor = convert(mocks.validOne[sourceName]);
+
+                const generateName = generatorsMap.get(sourceName);
+                const generate = colorverter[generateName].bind(colorverter);
+
+                const newColor = convert(generate());
                 expect(newColor instanceof Array).toBeTruthy();
             });
         });
@@ -101,26 +115,46 @@ describe('colorverter.*To*', () => {
 
 describe('colorverter.*To*', () => {
     it('should convert colors to expected value', () => {
-        convertersMap.forEach((targetsMap, sourceName) => {
-            targetsMap.forEach((convertName, targetName) => {
-                const convert = colorverter[convertName].bind(colorverter);
+        for (const testPair of testPairs) {
+            const from = testPair[0];
+            const fromVal = testPair[1];
+            const to = testPair[2];
+            const toVal = testPair[3];
 
-                const errorPrefix = `From ${sourceName} to ${targetName}: `;
+            const convertName = getConverter(from, to);
+            const convert = colorverter[convertName].bind(colorverter);
+            const generated = convert(fromVal);
 
-                let generated = convert(mocks.validOne[sourceName]);
-                let expected = mocks.validOne[targetName];
-                let isSame = colorverter.isSameColor(generated, expected);
-                expect(isSame).toBeTruthy(
-                    `${errorPrefix}expected ${expected}, but got ${generated}`
-                );
+            const isSame = colorverter.isSameColor(generated, toVal);
+            expect(isSame).toBeTruthy(
+                `From ${from} to ${to}: expected ${toVal}, got ${generated}`
+            );
+        }
+    });
+});
 
-                generated = convert(mocks.validTwo[sourceName]);
-                expected = mocks.validTwo[targetName];
-                isSame = colorverter.isSameColor(generated, expected);
-                expect(isSame).toBeTruthy(
-                    `${errorPrefix}expected ${expected}, but got ${generated}`
-                );
-            });
-        });
+describe('colorverter.hwbToRgb', () => {
+    it('should convert to black for extreme blackness', () => {
+        for (let angle = 0; angle <= 360; angle++) {
+            const newColor = colorverter.hwbToRgb([angle, 0, 100]);
+            const isSame = colorverter.isSameColor(newColor, [0, 0, 0]);
+            expect(isSame).toBeTruthy();
+        }
+    });
+
+    it('should convert to white for extreme whiteness', () => {
+        for (let angle = 0; angle <= 360; angle++) {
+            const newColor = colorverter.hwbToRgb([angle, 100, 0]);
+            const isSame = colorverter.isSameColor(newColor, [255, 255, 255]);
+            expect(isSame).toBeTruthy();
+        }
+    });
+
+    it('should convert to gray for extreme both parameters', () => {
+        for (let angle = 0; angle <= 360; angle++) {
+            const newColor = colorverter.hwbToRgb([angle, 100, 100]);
+            const isSame = colorverter.isSameColor(newColor, [128, 128, 128]);
+            expect(isSame).toBeTruthy();
+        }
     });
 });
