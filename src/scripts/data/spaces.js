d5ac3227fdd519d8478adaebf67c8eb28ead15bd
Leszek Pietrzak <smutnyleszek@gmail.com>
2016-10-04 09:35:52 +0200

    cleaned up spaces data object

--- src/scripts/data/spaces.js
+++ src/scripts/data/spaces.js
@@ -2,116 +2,98 @@ import deepFreeze from '../helpers/deepFreeze';
 
 const spacesData = deepFreeze({
     hex: {
-        slug: 'hex',
+        syntax: {
+            before: '#',
+            between: '',
+            after: ''
+        },
         properties: [
             {
                 name: 'red',
-                value: '00',
                 type: 'hexadecimal',
-                maxlength: 2,
-                syntaxBefore: '#'
+                length: 2
             },
             {
                 name: 'green',
-                value: '00',
                 type: 'hexadecimal',
-                maxlength: 2
+                length: 2
             },
             {
                 name: 'blue',
-                value: '00',
                 type: 'hexadecimal',
-                maxlength: 2
+                length: 2
             }
         ]
     },
     rgb: {
-        slug: 'rgb',
+        syntax: {
+            before: 'rgb(',
+            between: ',',
+            after: ')'
+        },
         properties: [
             {
                 name: 'red',
-                value: 0,
                 type: 'integer',
-                range: [0, 255],
-                maxlength: 3,
-                syntaxBefore: 'rgb(',
-                syntaxAfter: ', '
+                range: [0, 255]
             },
             {
                 name: 'green',
-                value: 0,
                 type: 'integer',
-                range: [0, 255],
-                maxlength: 3,
-                syntaxAfter: ', '
+                range: [0, 255]
             },
             {
                 name: 'blue',
-                value: 0,
                 type: 'integer',
-                range: [0, 255],
-                maxlength: 3,
-                syntaxAfter: ')'
+                range: [0, 255]
             }
         ]
     },
     hsl: {
-        slug: 'hsl',
+        syntax: {
+            before: 'hsl(',
+            between: ',',
+            after: ')'
+        },
         properties: [
             {
                 name: 'hue',
-                value: 0,
                 type: 'integer',
-                range: [0, 360],
-                maxlength: 3,
-                syntaxBefore: 'hsl(',
-                syntaxAfter: ', '
+                range: [0, 360]
             },
             {
                 name: 'saturation',
-                value: 0,
-                type: 'percentage',
-                range: [0, 100],
-                maxlength: 3,
-                syntaxAfter: '%, '
+                type: 'integer',
+                range: [0, 100]
             },
             {
                 name: 'lightness',
-                value: 0,
-                type: 'percentage',
-                range: [0, 100],
-                maxlength: 3,
-                syntaxAfter: '%)'
+                type: 'integer',
+                range: [0, 100]
             }
         ]
     },
     hwb: {
-        slug: 'hwb',
+        syntax: {
+            before: 'hwb(',
+            between: ',',
+            after: ')'
+        },
         properties: [
             {
                 name: 'hue',
-                value: 0,
                 type: 'integer',
-                range: [0, 360],
-                maxlength: 3,
-                syntaxBefore: 'hwb(',
-                syntaxAfter: ', '
+                range: [0, 360]
             },
             {
                 name: 'whiteness',
-                value: 0,
-                type: 'percentage',
-                range: [0, 100],
-                maxlength: 3,
-                syntaxAfter: '%, '
+                type: 'integer',
+                range: [0, 100]
             },
             {
                 name: 'blackness',
-                value: 0,
-                type: 'percentage',
-                range: [0, 100],
-                maxlength: 3,
-                syntaxAfter: '%)'
+                type: 'integer',
+                range: [0, 100]
             }
         ]
     }
